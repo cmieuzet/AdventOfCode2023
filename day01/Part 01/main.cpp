3e9d038a5e2de187fa1af9553fa546fc8d3d7d33
cmieuzet <corentin.mieuzet@gmail.com>
2023-12-02 22:18:02 +0100

    day01

--- day01/Part 01/main.cpp
+++ day01/Part 01/main.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
 #include <fstream>
 
-void	debug(const char & f, const char & l, const int & v) {
+void debug(const char &f, const char &l, const int &v)
+{
 
 	std::cout << "first : " << f;
 	std::cout << " | last : " << l;
 	std::cout << " | value : " << v << std::endl;
 }
 
-size_t	getNumberLine(const std::string & line) {
+size_t getNumberLine(const std::string &line)
+{
 
-	size_t	value = 0;
-	char	first;
-	char	last;
-	
+	size_t value = 0;
+	char first;
+	char last;
+
 	std::string::const_iterator it = line.begin();
 	std::string::const_iterator itEnd = line.end();
-	for (; it != itEnd; it++) {
-		if (std::isdigit(*it)) {
+	for (; it != itEnd; it++)
+	{
+		if (std::isdigit(*it))
+		{
 			last = *it;
 			if (!first)
 				first = *it;
 		}
 	}
-	value = (first - '0' )* 10 + (last - '0');
-//	debug(first, last, value);
+	value = (first - '0') * 10 + (last - '0');
+	//	debug(first, last, value);
 	return value;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-	std::string	line;
-	std::ifstream	file;
-	size_t		value = 0;
+	std::string line;
+	std::ifstream file;
+	size_t value = 0;
 
 	if (argc != 2)
 		return 1;
--- day01/Part 02/main.cpp
+++ day01/Part 02/main.cpp
@@ -1,39 +1,77 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <cstring>
 
-	std::map<std::string, int>	number;
+const std::map<std::string, std::pair<int, int>> &getMapNumberWord(void)
+{
 
-bool	findNumberWord(const std::string & word) {
+	static std::map<std::string, std::pair<int, int>> numberUtils;
 
-	std::cout << word << std::endl;
-	return true;
+	if (numberUtils.empty())
+	{
+
+		numberUtils["one"] = {3, 1};
+		numberUtils["two"] = {3, 2};
+		numberUtils["three"] = {5, 3};
+		numberUtils["four"] = {4, 4};
+		numberUtils["five"] = {4, 5};
+		numberUtils["six"] = {3, 6};
+		numberUtils["seven"] = {5, 7};
+		numberUtils["eight"] = {5, 8};
+		numberUtils["nine"] = {4, 9};
+	}
+	return numberUtils;
 }
 
-size_t	getNumberLine(const std::string & line) {
+int findNumberWord(const std::string &word)
+{
 
-	std::vector<int>	vec;
-	std::string::const_iterator	it = line.begin();
-	std::string::const_iterator	itEnd = line.end();
+	static const std::map<std::string, std::pair<int, int>> numberUtils = getMapNumberWord();
+	std::map<std::string, std::pair<int, int>>::const_iterator it = numberUtils.begin();
 
-	for (; it != itEnd; it++) {
-		if (std::isdigit(*it)) {
+	for (; it != numberUtils.end(); it++)
+	{
+
+		if (!strncmp(word.c_str(), it->first.c_str(), it->second.first))
+			return it->second.second;
+	}
+	return 0;
+}
+
+size_t getNumberLine(const std::string &line)
+{
+
+	int tmp;
+	std::vector<int> vec;
+	std::string::const_iterator it = line.begin();
+	std::string::const_iterator itEnd = line.end();
+
+	for (; it != itEnd; it++)
+	{
+		if (std::isdigit(*it))
+		{
 			vec.push_back(*it - '0');
-		} else if (findNumberWord(&*it)) {
-			//vec.push_back(getInt())
+		}
+		else
+		{
+			tmp = findNumberWord(&*it);
+			if (tmp != 0)
+				vec.push_back(tmp);
 		}
 	}
-
-	(void)line;
-	return 1;
+	return (*(vec.begin()) * 10) + (*(vec.end() - 1));
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-	std::string	line;
-	std::ifstream	file;
-	size_t		value = 0;
+	std::string line;
+	std::ifstream file;
+	size_t value = 0;
 
 	if (argc != 2)
 		return 1;
@@ -45,10 +83,3 @@ int main(int argc, char **argv) {
 	std::cout << "Answer : " << value << std::endl;
 	return 0;
 }
-/*
-zpcvkjjqeight94one82mrvfzdvlgjgkhj
-
-if *it isdigit push_back
-
-if *it match = singleton return 9
-*/
